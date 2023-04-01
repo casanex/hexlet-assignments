40ab6f07e91f66cc052078916b78124087af2fd7
@hexlet/cli <support@hexlet.io>
2023-04-01 22:34:40 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -8,8 +8,8 @@ class App {
     public static boolean scrabble(String symbols, String word) {
 
         int length = word.length();
-        String[] letterss = symbols.split("");
-        List coll = new ArrayList(Arrays.asList(letterss));
+        String[] letter = symbols.split("");
+        List coll = new ArrayList(Arrays.asList(letter));
 
         for (int i = 0; i < length; i++) {
             String current = word.substring(i, i + 1).toLowerCase();
