1ceea50d5e59abdfd051c39fc5a1cfb661a96f0b
@hexlet/cli <support@hexlet.io>
2023-04-24 19:41:14 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -11,12 +11,25 @@ import org.junit.jupiter.api.Test;
 
 class AppTest {
 
-    int count;
-    @BeforeEach
-
     @Test
     void testTake() {
-
-
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> expected1 = new ArrayList<>();
+        List<Integer> result1 = App.take(list1, 3);
+        assertThat(result1).isEqualTo(expected1);
+    }
+    @Test
+    void testTake2() {
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> expected2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> result2 = App.take(list2, 10);
+        assertThat(result2).isEqualTo(expected2);
+    }
+    @Test
+    void testTake3() {
+        List<Integer> list3 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> expected3 = new ArrayList<>(Arrays.asList(1, 2));
+        List<Integer> result3 = App.take(list3, 2);
+        assertThat(result3).isEqualTo(expected3);
     }
 }
